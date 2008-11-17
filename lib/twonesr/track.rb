436bf7b9f7e2a1e13ea55fa921cc1f40f4400784
Manfred Stienstra <manfred@fngtps.com>
2008-11-18 00:14:05 +0100

    Add simple track test.

--- lib/twonesr/track.rb
+++ lib/twonesr/track.rb
@@ -1,2 +1,12 @@
-class Track
+module Twonesr
+  class Track
+    ATTRIBUTES = [:creator, :title, :year, :album, :duration, :trackNum]
+    attr_accessor *ATTRIBUTES
+    
+    def initialize(attributes={})
+      ATTRIBUTES.each do |attribute|
+        send("#{attribute}=", attributes[attribute]) if attributes[attribute]
+      end
+    end
+  end
 end
